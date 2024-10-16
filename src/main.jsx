f71c4ea713d635345910bdd41cdad90243950012
nandhakumargk <inandhakumargk@gmail.com>
2024-10-17 01:22:48 +0530

    Fix deployment issues for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </StrictMode>,
-)
+  </Router>,
+  document.getElementById('root')
+);
